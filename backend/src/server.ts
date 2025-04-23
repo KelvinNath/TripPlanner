32ca63a2cb62f6a6b1cb60d29d894152b13e8e43
KelvinNath <kelvinnath0987@gmail.com>
2025-04-23 14:31:54 +0530

    Added new lines

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,6 +18,14 @@ app.use(cors({
     credentials: true // Allow credentials if needed
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+
 
 app.use(express.json());
 
